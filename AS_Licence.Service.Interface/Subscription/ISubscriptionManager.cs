1d4ce049d90ec6d054f1cee8a10518d95768e1d3
aligbayir <a.bayrgorkem@gmail.com>
2023-09-03 18:26:26 +0300

    update fonksiyonu eklendi

--- AS_Licence.Service.Interface/Subscription/ISubscriptionManager.cs
+++ AS_Licence.Service.Interface/Subscription/ISubscriptionManager.cs
@@ -1,4 +1,5 @@
-﻿using AS_Licence.Entities.ViewModel.Operations;
+﻿using AS_Licence.Entities.Model.CustomerComputerInfo;
+using AS_Licence.Entities.ViewModel.Operations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,25 +9,25 @@ using System.Threading.Tasks;
 
 namespace AS_Licence.Service.Interface.Subscription
 {
-  public interface ISubscriptionManager
-  {
-    Task<OperationResponse<List<Entities.Model.Subscription.Subscription>>> GetSubscriptionList(
-      Expression<Func<Entities.Model.Subscription.Subscription, bool>> filter = null,
-      Func<IQueryable<Entities.Model.Subscription.Subscription>,
-        IOrderedQueryable<Entities.Model.Subscription.Subscription>> orderBy = null, string includeProperties = "");
-    Task<OperationResponse<Entities.Model.Subscription.Subscription>> SaveSubscription(
-      Entities.Model.Subscription.Subscription subscription);
-    Task<OperationResponse<Entities.Model.Subscription.Subscription>> DeleteSubscriptionBySubscriptionId(int id);
-    Task<OperationResponse<Entities.Model.Subscription.Subscription>> GetSubscriptionBySubscriptionId(int id);
-    Task<OperationResponse<List<Entities.Model.Subscription.Subscription>>> GetSubscriptionListBySoftwareId(
-      int softwareId);
+    public interface ISubscriptionManager
+    {
+        Task<OperationResponse<List<Entities.Model.Subscription.Subscription>>> GetSubscriptionList(
+          Expression<Func<Entities.Model.Subscription.Subscription, bool>> filter = null,
+          Func<IQueryable<Entities.Model.Subscription.Subscription>,
+            IOrderedQueryable<Entities.Model.Subscription.Subscription>> orderBy = null, string includeProperties = "");
+        Task<OperationResponse<Entities.Model.Subscription.Subscription>> SaveSubscription(
+          Entities.Model.Subscription.Subscription subscription);
+        Task<OperationResponse<Entities.Model.Subscription.Subscription>> DeleteSubscriptionBySubscriptionId(int id);
+        Task<OperationResponse<Entities.Model.Subscription.Subscription>> GetSubscriptionBySubscriptionId(int id);
+        Task<OperationResponse<List<Entities.Model.Subscription.Subscription>>> GetSubscriptionListBySoftwareId(
+          int softwareId);
 
-
-    Task<OperationResponse<List<Entities.Model.Subscription.Subscription>>> GetSubscriptionListByCustomerId(
+        Task<OperationResponse<Entities.Model.Subscription.Subscription>> UpdateSubscription(Entities.Model.Subscription.Subscription model);
+        Task<OperationResponse<List<Entities.Model.Subscription.Subscription>>> GetSubscriptionListByCustomerId(
       int customerId);
-    Task<OperationResponse<Entities.Model.Subscription.Subscription>> GetBySubscriptionStatusBySoftwareIdAndCustomerId(
-      int softwareId, int customerId);
-    Task<OperationResponse<List<Entities.ViewModel.Subscription.SubscriptionInfo>>> GetSubscriptionSummaryListByCustomerId(
-      int customerId);
-  }
+        Task<OperationResponse<Entities.Model.Subscription.Subscription>> GetBySubscriptionStatusBySoftwareIdAndCustomerId(
+          int softwareId, int customerId);
+        Task<OperationResponse<List<Entities.ViewModel.Subscription.SubscriptionInfo>>> GetSubscriptionSummaryListByCustomerId(
+          int customerId);
+    }
 }
--- AS_Licence.WebUI.CoreAPI/Controllers/SubscriptionController.cs
+++ AS_Licence.WebUI.CoreAPI/Controllers/SubscriptionController.cs
@@ -3,73 +3,87 @@ using AS_Licence.Service.Interface.Subscription;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using AS_Licence.Entities.Model.CustomerComputerInfo;
+using AS_Licence.Service.Interface.Customer;
 
 namespace AS_Licence.WebUI.CoreAPI.Controllers
 {
-  [Route("api/[controller]")]
-  [ApiController]
-  [Authorize]
-  public class SubscriptionController : ControllerBase
-  {
-    private ISubscriptionManager _subscriptionManager;
-
-    public SubscriptionController(ISubscriptionManager subscriptionManager)
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class SubscriptionController : ControllerBase
     {
-      _subscriptionManager = subscriptionManager;
+        private ISubscriptionManager _subscriptionManager;
+
+        public SubscriptionController(ISubscriptionManager subscriptionManager)
+        {
+            _subscriptionManager = subscriptionManager;
+        }
+
+
+        [HttpPost]
+        [Route("SaveSubscription")]
+        public async Task<IActionResult> SaveSubscription([FromBody] Subscription subscription)
+        {
+            var subscriptionResult = await _subscriptionManager.SaveSubscription(subscription);
+            if (subscriptionResult.Status == false)
+            {
+                return BadRequest(subscriptionResult);
+            }
+
+            return Ok(subscriptionResult);
+        }
+        //[HttpPut]
+        //[Route("UpdateSubscription")]
+        //public async Task<IActionResult> Put([FromBody] CustomerComputerInfo customerComputerInfo)
+        //{
+        //    var customerComputerInfoResult = await _customerComputerInfoManager.UpdateCustomerComputerInfo(customerComputerInfo);
+        //    if (customerComputerInfoResult.Status == false)
+        //    {
+        //        return BadRequest(customerComputerInfoResult);
+        //    }
+
+        //    return Ok(customerComputerInfoResult);
+        //}
+
+        [HttpGet]
+        [Route("GetSubscriptionSummaryListByCustomerId")]
+        public async Task<IActionResult> GetSubscriptionSummaryListByCustomerId(int customerId)
+        {
+
+            var subscriptionResult = await _subscriptionManager.GetSubscriptionSummaryListByCustomerId(customerId);
+            if (subscriptionResult.Status == false)
+            {
+                return BadRequest(subscriptionResult);
+            }
+
+            return Ok(subscriptionResult);
+        }
+
+        [HttpGet]
+        [Route("GetSubscriptionBySubscriptionId")]
+        public async Task<IActionResult> GetSubscriptionBySubscriptionId(int subscriptionId)
+        {
+            var subscriptionResult = await _subscriptionManager.GetSubscriptionBySubscriptionId(subscriptionId);
+            if (subscriptionResult.Status == false)
+            {
+                return BadRequest(subscriptionResult);
+            }
+
+            return Ok(subscriptionResult);
+        }
+
+        [HttpDelete]
+        [Route("DeleteSubscriptionBySubscriptionId/{subscriptionId}")]
+        public async Task<IActionResult> DeleteSubscriptionBySubscriptionId(int subscriptionId)
+        {
+            var subscriptionResult = await _subscriptionManager.DeleteSubscriptionBySubscriptionId(subscriptionId);
+            if (subscriptionResult.Status == false)
+            {
+                return BadRequest(subscriptionResult);
+            }
+
+            return Ok(subscriptionResult);
+        }
     }
-
-
-    [HttpPost]
-    [Route("SaveSubscription")]
-    public async Task<IActionResult> SaveSubscription([FromBody] Subscription subscription)
-    {
-      var subscriptionResult = await _subscriptionManager.SaveSubscription(subscription);
-      if (subscriptionResult.Status == false)
-      {
-        return BadRequest(subscriptionResult);
-      }
-
-      return Ok(subscriptionResult);
-    }
-
-    [HttpGet]
-    [Route("GetSubscriptionSummaryListByCustomerId")]
-    public async Task<IActionResult> GetSubscriptionSummaryListByCustomerId(int customerId)
-    {
-      
-      var subscriptionResult = await _subscriptionManager.GetSubscriptionSummaryListByCustomerId(customerId);
-      if (subscriptionResult.Status == false)
-      {
-        return BadRequest(subscriptionResult);
-      }
-
-      return Ok(subscriptionResult);
-    }
-
-    [HttpGet]
-    [Route("GetSubscriptionBySubscriptionId")]
-    public async Task<IActionResult> GetSubscriptionBySubscriptionId(int subscriptionId)
-    {
-      var subscriptionResult = await _subscriptionManager.GetSubscriptionBySubscriptionId(subscriptionId);
-      if (subscriptionResult.Status == false)
-      {
-        return BadRequest(subscriptionResult);
-      }
-
-      return Ok(subscriptionResult);
-    }
-
-    [HttpDelete]
-    [Route("DeleteSubscriptionBySubscriptionId/{subscriptionId}")]
-    public async Task<IActionResult> DeleteSubscriptionBySubscriptionId(int subscriptionId)
-    {
-      var subscriptionResult = await _subscriptionManager.DeleteSubscriptionBySubscriptionId(subscriptionId);
-      if (subscriptionResult.Status == false)
-      {
-        return BadRequest(subscriptionResult);
-      }
-
-      return Ok(subscriptionResult);
-    }
-  }
 }
